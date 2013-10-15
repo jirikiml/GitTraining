aec4127b49b8ee8e9689f7925e2f4829daac0a0b
Kiml <Kiml@192.168.100.26>
2013-10-15 11:54:54 +0200

    small change in equals (prepare for merge conflict)

--- GitTraining/src/com/cg/ihe/doc/domain/DomainId.java
+++ GitTraining/src/com/cg/ihe/doc/domain/DomainId.java
@@ -80,7 +80,7 @@ public class DomainId implements Serializable {
      */
     @Override
     public boolean equals(Object obj) {
-        if (this == obj) {
+        if (this != obj) {
             return true;
         }
         if (obj == null) {
